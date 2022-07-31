659cd2a6296a4fd2a097e1509eca7f02567af55c
Yb.M <Yb.M@DESKTOP-95HFT22>
2022-07-30 22:34:40 -0500

    se añadió algo para probar los cambios hechos

---
+++ src/proyectFrmClass/classParaPruebaDeCambios.java
@@ -0,0 +1,12 @@
+package proyectFrmClass;
+
+public class classParaPruebaDeCambios {
+    public static void main(String[] args) {
+        int cont = 0;
+        
+        while (cont < 3) {            
+            cont++;
+            System.out.println("PRUEBA FINALIZADA");
+        }
+    }
+}
