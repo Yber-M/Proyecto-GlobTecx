a4317a9bfcbdeb9ffcd98affb2d968007e4e38bd
{Yb./M} <93751686+Yber-M@users.noreply.github.com>
2022-07-30 23:06:57 -0500

    AGREGAMOS

--- src/proyectFrmClass/hjsColores.java
+++ src/proyectFrmClass/hjsColores.java
@@ -5,7 +5,7 @@ public class hjsColores extends pdrUtiles {
     public hjsColores(int posicion, String producto, int cant) {
         super(posicion, producto, cant);
     }
-
+    System.out.print("PRUEBA DE QUE EXTRAEMOS LO CAMBIOS");
     public void PrecioUni() {
         switch (producto) {
             case "Colores FaberCastle":
